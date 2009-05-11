76e4fcba8092fed5cb2f9bf1232aab49abfa8ab2
Felix Langfeldt <felix@neverwoods.com>
2009-05-11 01:43:15 +0000

    Updated the Export2Html method.

--- manager/_tools/export2html.php
+++ manager/_tools/export2html.php
@@ -1,7 +1,7 @@
 <?php
 
 /**************************************************************************
-* PunchCMS Export2Html script v0.1.0
+* PunchCMS Export2Html script v.0.2.0
 * Exports an entire site to plain HTML files, including the used folders.
 **************************************************************************/
 
@@ -9,28 +9,28 @@ require_once('includes/init.php');
 
 $strBaseFolder 		= $_CONF['app']['basePath'];
 $strZipFolder 		= $_CONF['app']['basePath'] . "../cache/";
-$objZip 			= new ZipArchive();
+$strZipName 		= $strZipFolder . "exportZip_" . rand() . ".zip";
+$objZip 			= new dZip($strZipName, TRUE);
 $blnDebug			= FALSE;
 
 function copyr($source, $dest, $fileFilter = array()){
-	global $objZip,
-		$blnZipCreated;
-		
+	global $objZip;
+
 	//*** Simple copy for a file.
 	if (is_file($source)) {
 		if (count($fileFilter) == 0 || in_array(extension($source), $fileFilter)) {
-			if ($blnZipCreated === TRUE) $objZip->addFile($source, $dest);
+			$objZip->addFile($source, $dest);
 			logExport($source);
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	if (!is_dir($source)) {
 		return false;
 	}
-	
+
 	//*** Loop through the folder
 	$dir = dir($source);
 	while (false !== $entry = $dir->read()) {
@@ -44,9 +44,9 @@ function copyr($source, $dest, $fileFilter = array()){
 			copyr("$source/$entry", "$dest/$entry", $fileFilter);
 		}
 	}
-	
+
 	logExport($source);
-	
+
 	//*** Clean up
 	$dir->close();
 	return true;
@@ -60,15 +60,15 @@ function extension($strPath) {
 
 function logExport($strLog) {
 	global $blnDebug;
-	
+
 	if ($blnDebug) echo "(" . strftime("%Y-%m-%d %H:%M:%S") . ") exported: " . $strLog . "<br />";
 }
 
 function fixLinks($strInput) {
 	$objCms = PCMS_Client::getInstance();
 	$strOutput = $strInput;
-	
-	//*** Transform old skool ?eid= URLs to /eid/ URLs.				
+
+	//*** Transform old skool ?eid= URLs to /eid/ URLs.
 	$strPattern = "/href=\"(\/)*(\?eid=)([0-9]+)/ie";
 	$arrMatches = array();
 	if (preg_match_all($strPattern, $strOutput, $arrMatches) > 0) {
@@ -80,7 +80,7 @@ function fixLinks($strInput) {
 			}
 		}
 	}
-	
+
 	//*** Transform download.php?eid= URLs to the direct file URLs.
 	preg_match_all("/href=\"download.php\?eid=([0-9]+)(&index=([0-9]+))*\"/", $strOutput, $arrMatches);
 	if (count($arrMatches) > 1) {
@@ -93,75 +93,69 @@ function fixLinks($strInput) {
 				if (is_array($arrFiles) && count($arrFiles) > $intIndex) {
 					$arrValue = $arrFiles[$intIndex];
 					$strTarget = $objCms->getFilePath() . $arrValue['src'];
-				
+
 					$strOutput = str_replace($arrMatches[0][$intCount], "href=\"{$strTarget}\"", $strOutput);
 				}
 			}
 		}
 	}
-	
+
 	//*** Transform aliases to .html files.
 	$strOutput = ereg_replace("href=\"(/[a-zA-Z0-9_/-]+)\"", "href=\"\\1.html\"", $strOutput);
-	
+
 	return $strOutput;
 }
 
 function exportIndexPage($objLanguage) {
-	global $objZip,
-		$blnZipCreated;
-	
+	global $objZip;
+
 	$strDirname = (!$objLanguage->default) ? "language/" . $objLanguage->getAbbr() : "";
-	$strBasename = "index.html";	
-	
+	$strBasename = "index.html";
+
 	$strFile = file_get_contents(Request::getRootURI() . "/language/" . $objLanguage->getAbbr());
-	if ($blnZipCreated === TRUE) $objZip->addFromString($strDirname . "/" . $strBasename, fixLinks($strFile));
-	
+	$objZip->addFile($strDirname . "/" . $strBasename, '', '', fixLinks($strFile));
+
 	logExport($strDirname . "/" . $strBasename);
 }
 
 function exportPage($objPageElement, $objLanguage) {
-	global $objZip,
-		$blnZipCreated;
-	
+	global $objZip;
+
 	$objCms = PCMS_Client::getInstance();
-	
+
 	$strDirname = (!$objLanguage->default) ? dirname($objPageElement->getLink(TRUE, "", $objLanguage->getAbbr())) : dirname($objPageElement->getLink());
 	$strBasename = basename($objPageElement->getLink());
-	
+
 	if ($strDirname == "/") $strDirname = "";
-	
+
 	$strFile = file_get_contents(Request::getRootURI() . $objPageElement->getLink());
-	if ($blnZipCreated === TRUE) $objZip->addFromString($strDirname . "/" . $strBasename . ".html", fixLinks($strFile));
-	
+	$objZip->addFile($strDirname . "/" . $strBasename . ".html", '', '', fixLinks($strFile));
+
 	logExport($strDirname . "/" . $strBasename);
 }
 
 function exportSite() {
-	global $strBaseFolder, 
-		$strZipFolder, 
-		$objZip,
-		$blnZipCreated;
-	
+	global $strBaseFolder,
+		$strZipFolder,
+		$strZipName,
+		$objZip;
+
 	$objCms = PCMS_Client::getInstance();
 	$objCms->setLanguage(ContentLanguage::getDefault());
-		
-	//*** Create zip file.
-	$strZipName = $strZipFolder . "exportZip_" . rand() . ".zip";
-	$blnZipCreated = $objZip->open($strZipName, ZipArchive::CREATE);
 
 	//*** Get a collection of Languages.
 	$objLanguages = $objCms->getLanguages();
 	foreach ($objLanguages as $objLanguage) {
 		//*** Export the entry page.
 		exportIndexPage($objLanguage);
-	
+
 		//*** Get the list of pages.
 		$objElements = $objCms->getPageElements($objLanguage->getId());
 		foreach ($objElements as $objElement) {
 			exportPage($objElement, $objLanguage);
 		}
 	}
-	
+
 	//*** Export auxilary fodlers.
 	$arrFileFilter = array("js", "css", "jpg", "gif", "png", "ico", "txt", "html");
 	copyr($strBaseFolder . "css", "css", $arrFileFilter);
@@ -171,10 +165,10 @@ function exportSite() {
 	copyr($strBaseFolder . "libraries", "libraries", $arrFileFilter);
 	copyr($strBaseFolder . "favicon.ico", "favicon.ico");
 	copyr($strBaseFolder . "robots.txt", "robots.txt");
-	
+
 	//*** Close zip file.
-    $objZip->close();
-    
+    $objZip->save();
+
     //*** Return the zip file to the user.
 	header("HTTP/1.1 200 OK");
 	header("Pragma: public");
