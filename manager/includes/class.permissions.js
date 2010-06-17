9524f51ad8221a862df06e947967311ff2b24df5
Robin van Baalen <robin@neverwoods.com>
2010-06-17 19:11:14 +0000

    Converted almost all Prototype javascript to jQuery. This class is still an alfa version and is not yet in use.

--- manager/includes/class.permissions.js
+++ manager/includes/class.permissions.js
@@ -1,29 +1,3 @@
-
-/*
-###########################################################################
-#  Copyright (c) 2006 Phixel.org (http://www.phixel.org)
-#
-#  Permission is hereby granted, free of charge, to any person obtaining
-#  a copy of this software and associated documentation files (the
-#  "Software"), to deal in the Software without restriction, including
-#  without limitation the rights to use, copy, modify, merge, publish,
-#  distribute, sublicense, and/or sell copies of the Software, and to
-#  permit persons to whom the Software is furnished to do so, subject to
-#  the following conditions:
-#
-#  The above copyright notice and this permission notice shall be
-#  included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-#  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-#  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-#  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-###########################################################################
-*/
-
 /**************************************************************************
  * PermissionList Class.
  *************************************************************************/
@@ -71,16 +45,16 @@ PermissionList.unsetKey = function(event) {
 
 PermissionList.untoggleList = function(event) {
 	if (!PermissionList.hasCtrl) {
-		jQuery('div.selectList a').each(function(){
-			jQuery(this).removeClass('on');
+		jQuery("div.selectList a").each(function(){
+			jQuery(this).removeClass("on");
 		});
 		PermissionList.clearPermissions();
 	}
 }
 
 PermissionList.toggleList = function(event) {
-	var blnShow = true;
-	var objElement = Event.element(event);
+	var blnShow 	= true;
+	var objElement 	= Event.element(event);
 	
 	if (objElement.hasClassName('on')) blnShow = false;
 	PermissionList.untoggleList(event);
