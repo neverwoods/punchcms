3234a0942030d40e6ee43a6a0202addc9a8c0ed0
Robin van Baalen <robin@neverwoods.com>
2013-07-06 02:04:32 +0000

    Vulnerability patches

--- renewed-upload-system/thumb.php
+++ renewed-upload-system/thumb.php
@@ -2,11 +2,21 @@
 
 require_once('includes/init.php');
 
+//*** Only logged in users are allowed to make thumbs
+require_once('includes/inc.login.php');
+
 $src 			= request('src');
 $strExtension 	= substr(strrchr($src, '.'), 1);
 $strHash		= md5($src) . "." . $strExtension;
 
-@file_put_contents($_PATHS['upload'] . $strHash, file_get_contents($src));
-ImageResizer::resize($_PATHS['upload'] . $strHash, 15, 11, RESIZE_DISTORT, 60, TRUE, NULL, TRUE);
+$arrExtensionWhiteList = array(
+	"jpg", "jpeg", "png", "gif"
+);
 
-?>
+if (in_array($strExtension, $arrExtensionWhiteList)) {
+	@file_put_contents($_PATHS['upload'] . $strHash, file_get_contents($src));
+	ImageResizer::resize($_PATHS['upload'] . $strHash, 15, 11, RESIZE_DISTORT, 60, true, null, true);
+} else {
+	// Fallback image
+
+}
