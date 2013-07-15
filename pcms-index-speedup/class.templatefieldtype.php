dcaa561ccdddc22c7c0628efebb4dcb207a6099d
Mark Kappert <mark@spin.cw>
2013-07-15 16:53:14 +0000

    copy trunk/client to a branch

---
+++ pcms-index-speedup/class.templatefieldtype.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 
+ * Handles TemplateFieldType properties and methods.
+ * @author felix
+ * @version 0.1.0
+ *
+ */
+class TemplateFieldType extends DBA_TemplateFieldType {
+
+}
+
+?>
