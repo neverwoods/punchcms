0f803a9a33b21eb2058ee9c794642fd1e36b6dd4
Robin van Baalen <robin@neverwoods.com>
2013-07-23 15:43:10 +0000

    Fixed SQL injection vulnerability

--- renewed-upload-system/ajaxtree.php
+++ renewed-upload-system/ajaxtree.php
@@ -2,11 +2,16 @@
 
 require_once('includes/init.php');
 
+//*** Only logged in users are allowed to make AJAX requests
+require_once('includes/inc.login.php');
+
 $eId 		= Request::get('id', 0);
 $type 		= Request::get('type', "elements");
 $cmd 		= Request::get('cmd', "list");
 
-if ($eId == "-1") $eId = 0;
+if ($eId == "-1" || !is_numeric($eId)) {
+    $eId = 0;
+}
 $strReturn 	= Tree::buildXmlTree($eId, $type, $cmd, $eId);
 
 header('Content-type: text/xml');
