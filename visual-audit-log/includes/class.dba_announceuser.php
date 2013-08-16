349b5d6aed278dc686572bf59a59124d0129ea3f
Robin van Baalen <robin@neverwoods.com>
2013-08-16 15:51:52 +0000

    Created Visual Audit Log branch

---
+++ visual-audit-log/includes/class.dba_announceuser.php
@@ -0,0 +1,64 @@
+<?php
+
+/***
+ *
+ * AnnounceUser DBA Class.
+ *
+ */
+
+class DBA_AnnounceUser extends DBA__Object {
+	protected $id = NULL;
+	protected $messageid = 0;
+	protected $permuserid = 0;
+
+	//*** Constructor.
+	public function DBA_AnnounceUser() {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+	}
+
+	//*** Static inherited functions.
+	public static function selectByPK($varValue, $arrFields = array(), $accountId = NULL) {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::selectByPK($varValue, $arrFields, $accountId);
+	}
+
+	public static function select($strSql = "") {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::select($strSql);
+	}
+
+	public static function doDelete($varValue) {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::doDelete($varValue);
+	}
+
+	public function save($blnSaveModifiedDate = TRUE) {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::save($blnSaveModifiedDate);
+	}
+
+	public function delete($accountId = NULL) {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::delete($accountId);
+	}
+
+	public function duplicate() {
+		self::$__object = "AnnounceUser";
+		self::$__table = "pcms_announce_user";
+
+		return parent::duplicate();
+	}
+}
+
+?>
---
+++ visual-audit-log/includes/class.group.js
@@ -0,0 +1,54 @@
+
+/************************
+* Group Class.
+*
+* Note:
+*   Requires the "prototype" library.
+**/
+
+function Group() {	
+	//*** Construct the User class;
+}
+
+Group.remove = function(intId) {
+	var strUrl = "ajax.php";
+	var strPost = {cmd: "Group::remove", group_id: intId };
+
+	jQuery("#userProgress").fadeIn("fast");
+	var request = jQuery.get(strUrl, strPost, Group.refresh, "xml");
+}
+
+Group.load = function(intId) {
+	var strUrl = "ajax.php";
+	var strPost = "cmd=Group::load&group_id=" + intId;
+
+	jQuery("#userProgress").fadeIn("fast");
+	var request = jQuery.get(strUrl, strPost, Group.show, "xml");
+}
+
+Group.write = function(strFormId) {
+	var strUrl = "ajax.php";
+	var strPost = Forms.serialize(strFormId) + "&cmd=Group::add";
+
+	jQuery("#userProgress").fadeIn("fast");
+	var request = jQuery.post(strUrl, strPost, Group.refresh, "xml");
+}
+
+Group.show = function(objResponse, strHeader) {
+	jQuery("#userProgress").fadeOut("fast", function(){
+		Forms.parseAjaxResponse(objResponse);
+	});
+}
+
+Group.refresh = function(objResponse, strHeader) {
+	history.go(0);
+}
+
+Group.clearForm = function(strForm) {
+	var strUrl = "ajax.php";
+	var strPost = { cmd: "Group::clearForm" };
+
+	Forms.clear(strForm, ["right_level"]);
+	jQuery("#userProgress").fadeIn("fast");
+	var request = jQuery.get(strUrl, strPost, Group.show, "xml");
+}
