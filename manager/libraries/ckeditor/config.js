9b525e1add842b6cb13191ff53cc5f4eebe8e5d9
Mark Kappert <mark@spin.cw>
2013-12-10 14:48:15 +0000

    added h1, h2, h3 support in ckeditor

--- manager/libraries/ckeditor/config.js
+++ manager/libraries/ckeditor/config.js
@@ -8,13 +8,20 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
     config.forcePasteAsPlainText = true;
     config.toolbar = [
-                ['Source'],
+                ['Source', 'Maximize'],
                 ['Cut','Copy','PasteText','PasteFromWord','-','Table'],
                 ['Undo','Redo','-','Find','Replace','-','Link','Unlink','Anchor','-','SpecialChar'],
-                ['Bold','Italic','-','Subscript','Superscript','-','NumberedList','BulletedList','-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','TextColor','FontSize','RemoveFormat', 'Maximize']
+                ['Bold','Italic','-','Subscript','Superscript','-','NumberedList','BulletedList','-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','RemoveFormat'],
+                ['Format','FontSize','TextColor']
             ];
-    config.width = 500;
-    config.filebrowserWindowWidth = '335';
-    config.filebrowserWindowHeight = '480';
+    config.format_tags = 'p;h1;h2;h3';
+    config.resize_enabled = true;
+    config.resize_dir = 'both';
+    config.width = 600;
+    config.resize_minWidth = 500;
+    config.resize_maxWidth = 1200;
+    config.resize_maxHeight = 600;
+    config.filebrowserWindowWidth = 335;
+    config.filebrowserWindowHeight = 480;
 
 };
